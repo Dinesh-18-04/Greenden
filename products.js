6fba8f74e1c7d3f9ab8bf15e0ed0a0d5591819a0
Dinesh-18-04 <dineshkumars.22cse@kongu.edu>
2024-10-21 23:37:58 +0530

    updated search bar

--- products.js
+++ products.js
@@ -10,3 +10,24 @@ close.addEventListener("click",function(event){
     console.log(event);
     sidenav.style.right= "-75%" ;
 })
+
+var search = document.getElementById("search");
+var container = document.getElementById("container");
+var card = container.querySelectorAll("div");
+// console.log(card);
+
+search.addEventListener("keyup",function(){
+    var enter = event.target.value.toUpperCase();
+    // console.log(card[0].querySelector("h1").textContent);
+    for(count=0;count<card.length;count=count+1){
+        var productname = card[count].querySelector("h1").textContent;
+        // console.log(productname);
+        if(productname.toUpperCase().indexOf(enter)<0){
+            card[count].style.display="none";
+        }
+        else{
+            card[count].style.display="block";
+        }
+    }
+
+})
